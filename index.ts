eb9d2c66a9f177c1751e0baa53f0d63d6095f61d
AMBERIAZ <amberriaz353@gmail.com>
2024-05-30 11:35:08 -0700

    adding my cli number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\nWelcome to Amber's cli number guessing game.");
+const randomNumber = Math.floor(Math.random() * 7 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuessedNumber",
+        message: "Enter your number (number limit from 1 to 7):",
+    },
+]);
+
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("\n\nCongratulations! You guessed the correct number.");
+} else {
+    console.log("\n\nSorry! You guessed the wrong number.");
+};
